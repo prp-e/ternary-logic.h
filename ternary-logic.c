ef088cdad764158ab25dd3957eef440a6597062c
Muhammadreza Haghiri <haghiri75@gmail.com>
2018-10-11 22:05:46 +0330

    And and Or functions changed

--- ternary-logic.c
+++ ternary-logic.c
@@ -5,61 +5,9 @@ ternary negate(ternary a){
 }
 
 ternary t_and(ternary a, ternary b){
-    if(a == TRUE && b == TRUE){
-        return TRUE; 
-    }
-    else if(a == TRUE && b == MAYBE){
-        return MAYBE; 
-    }
-    else if(a == TRUE && b == FALSE){
-        return FALSE; 
-    }
-    else if(a == MAYBE && b == TRUE){
-        return MAYBE; 
-    }
-    else if(a == MAYBE && b == MAYBE){
-        return MAYBE; 
-    }
-    else if(a == MAYBE && b == FALSE){
-        return FALSE;
-    }
-    else if(a == FALSE, b == TRUE){
-        return FALSE; 
-    }
-    else if(a == FALSE, b == MAYBE){
-        return FALSE; 
-    }
-    else {
-        return FALSE; 
-    }
+   return a < b ? a : b; 
 }
 
 ternary t_or(ternary a, ternary b){
-    if(a == TRUE && b == TRUE){
-        return TRUE; 
-    }
-    else if(a == TRUE && b == MAYBE){
-        return TRUE; 
-    }
-    else if(a == TRUE && b == FALSE){
-        return TRUE; 
-    }
-    else if(a == MAYBE && b == TRUE){
-        return TRUE; 
-    }
-    else if(a == MAYBE && b == MAYBE){
-        return MAYBE; 
-    }
-    else if(a == MAYBE && b == FALSE){
-        return MAYBE; 
-    }
-    else if(a == FALSE && b == TRUE){
-        return TRUE;
-    }
-    else if(a == FALSE && b == MAYBE){
-        return MAYBE; 
-    }
-    else {
-        return FALSE;
-    }
+    return a > b? a : b; 
 }
